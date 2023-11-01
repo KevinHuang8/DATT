d39c58d4d33078d5ad541bdb2032a373bb10003d
rwik2000 <rwik2000@gmail.com>
2023-11-01 00:34:12 -0700

    changing model names

--- quadsim/controllers/cntrl_config_presets.py
+++ quadsim/controllers/cntrl_config_presets.py
@@ -6,13 +6,13 @@ datt_hover_config = DATTConfig()
 
 # Simple DATT w/ feedforward, without any adaptation
 datt_config = DATTConfig()
-datt_config.policy_name = 'traj_mixed2D_all_refs_diffaxis2_17500000_steps.zip'
+datt_config.policy_name = 'datt_adaptive.zip'
 datt_config.task = DroneTask.TRAJFBFF
 
 
 # DATT w/ feedforward and L1 adaptation
 datt_adaptive_L1_config = DATTConfig()
-datt_adaptive_L1_config.policy_name = 'traj_mixed2D_wind_adaptive2_REAL'
+datt_adaptive_L1_config.policy_name = 'datt_wind_adaptive'
 datt_adaptive_L1_config.task = DroneTask.TRAJFBFF
 datt_adaptive_L1_config.adaptive = True
 datt_adaptive_L1_config.adaptation_type = 'l1'
@@ -20,11 +20,11 @@ datt_adaptive_L1_config.adaptive_policy_name = None
 
 # DATT w/ feedforward and RMA adaptation
 datt_adaptive_RMA_config = DATTConfig()
-datt_adaptive_RMA_config.policy_name = 'traj_mixed2D_wind_adaptive2_REAL'
+datt_adaptive_RMA_config.policy_name = 'datt_wind_adaptive'
 datt_adaptive_RMA_config.task = DroneTask.TRAJFBFF
 datt_adaptive_RMA_config.adaptive = True
 datt_adaptive_RMA_config.adaptation_type = 'rma'
-datt_adaptive_RMA_config.adaptive_policy_name = 'wind_adaptation_net_RMA'
+datt_adaptive_RMA_config.adaptive_policy_name = 'wind_RMA'
 
 
 pid_config = PIDConfig()
